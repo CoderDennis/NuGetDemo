1e1b97397ad535d3a51424fe1815fa5d2c8838f9
Dennis Palmer <dennis@rightnow.org>
2012-11-08 17:35:43 -0600

    initial commit

---
+++ NuGetDemo.Tests/ServiceTest.vb
@@ -0,0 +1,14 @@
+﻿Imports NUnit.Framework
+
+<TestFixture>
+Public Class ServiceTest
+
+    <Test>
+    Public Sub DataIsRight()
+        Dim people = MyService.GetPeople
+        Assert.AreEqual(1, people.Count)
+        Assert.AreEqual("Dennis Palmer", people.First.Key)
+        Assert.AreEqual("CoderDennis", people.First.Value)
+    End Sub
+
+End Class
---
+++ NuGetDemo/MyService.vb
@@ -0,0 +1,11 @@
+﻿Public Class MyService
+
+    Public Shared Function GetPeople() As Dictionary(Of String, String)
+
+        Dim dataXml = XDocument.Load("data.xml")
+        Return dataXml...<person>.ToDictionary(Function(p) p.<name>.Value, Function(p) p.<twitter>.Value)
+
+    End Function
+
+
+End Class
